2ba2a37d5b2a9bd7a06d4b20ec7dfd8e295d1fc1
Alexey Shirokov <shirokovam@lad24.ru>
2021-08-20 22:27:42 +0300

    legend page

---
+++ client/components/OneLegendPage/index.tsx
@@ -0,0 +1,13 @@
+import Header from '../Common/Header';
+import PageHeader from './PageHeader';
+
+const OneLegendPage = () => {
+  return (
+    <div className="container">
+      <Header />
+      <PageHeader />
+    </div>
+  );
+};
+
+export default OneLegendPage;
