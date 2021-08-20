b13335516e677b1c281c7e2c57502b66ff04ba51
Michael Belov <mikle0706@icloud.com>
2021-08-20 20:02:53 +0300

    header

--- client/components/MainPage/PageHeader/index.tsx
+++ client/components/MainPage/PageHeader/index.tsx
@@ -6,6 +6,13 @@ const PageHeader = () => {
     <div className={`${style.page__header_wrapper}`}>
       <div className={`${style.container} ${style.content__header}`}>
         <div className={style.description__title}>Journeys of Inspiration</div>
+        <div className={style.page__header_title}>
+          Start Your Journey <br /> for Enjoy Life.
+        </div>
+        <div className={style.header__description}>
+          Travel Agency Threats and Opportunities. The Perspective of <br />
+          Successful Owners
+        </div>
       </div>
     </div>
   );
