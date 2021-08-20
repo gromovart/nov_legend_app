4d44bb445eb150056a19589938f5068d7950064d
Michael Belov <mikle0706@icloud.com>
2021-08-20 19:44:26 +0300

    дизайн main page

--- client/components/MainPage/PageHeader/index.tsx
+++ client/components/MainPage/PageHeader/index.tsx
@@ -5,7 +5,7 @@ const PageHeader = () => {
   return (
     <div className={`${style.page__header_wrapper}`}>
       <div className={`${style.container} ${style.content__header}`}>
-        <div className={style.}>Journeys of Inspiration</div>
+        <div className={style.description__title}>Journeys of Inspiration</div>
       </div>
     </div>
   );
