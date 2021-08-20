c77666f808357a8769073274e27f51e7a7883f15
Alexey Shirokov <shirokovam@lad24.ru>
2021-08-21 02:57:39 +0300

    add fonts on app

--- client/components/MainPage/PageHeader/index.tsx
+++ client/components/MainPage/PageHeader/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { Button, notification, Popover } from 'antd';
-import { PlayCircleOutlined } from '@ant-design/icons';
+import { PlayCircleOutlined, CloseCircleOutlined } from '@ant-design/icons';
 import style from './styled.module.scss';
 
 const PageHeader = () => {
@@ -37,12 +37,32 @@ const PageHeader = () => {
     <div className={`${style.page__header_wrapper}`}>
       <div className={`container ${style.content__header}`}>
         {isPlayerVisible ? (
-          <audio controls>
-            <source src="/gusli.mp3" type="audio/mpeg" />
-          </audio>
+          <div className={style.content__header__player}>
+            <audio controls>
+              <source src="/gusli.mp3" type="audio/mpeg" />
+            </audio>
+            <Button
+              onClick={() => {
+                setIsPlayerVisible((prev) => !prev);
+              }}
+            >
+              <CloseCircleOutlined />
+            </Button>
+          </div>
         ) : (
-          <Popover content={"Xnj ,s"} title="Музыкальное сопровождение">
-            <PlayCircleOutlined className={style.content__header__icon} />
+          <Popover
+            content="Нажмите для воспроизведения музыкального сопровождения"
+            title=""
+            placement="bottomLeft"
+          >
+            <Button
+              className={style.content__header__btn}
+              onClick={() => {
+                setIsPlayerVisible((prev) => !prev);
+              }}
+            >
+              <PlayCircleOutlined className={style.content__header__icon} />
+            </Button>
           </Popover>
         )}
         <div>
