7333cea2423dd4ede369a46bebb383cb68a1fcca
Michael Belov <mikle0706@icloud.com>
2021-08-21 09:10:28 +0300

    исправил стиль sidebar

--- client/components/Common/Map/MapSidebar/SidebarMenu/CategoriesMenu/ActualMenuItem/index.tsx
+++ client/components/Common/Map/MapSidebar/SidebarMenu/CategoriesMenu/ActualMenuItem/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import style from './style.module.scss';
 
-const ActualMenuItem = () => {
+const ActualMenuItem = ({ title, year, percent }) => {
   return (
     <div className={style.card__wrapper}>
-      <div className={style.card__title}>Собор Софии Премудрости Божией</div>
-      <div className={style.card__description}>1998 г.</div>
+      <div className={style.card__title}>{title}</div>
+      <div className={style.card__description}>{year}</div>
       <div className={style.card__description}>Популярность:</div>
-      <div className={style.popular__line} />
+      <div className={style.popular__line}>
+        <div className={style.line__orange} style={{ width: percent }} />
+      </div>
     </div>
   );
 };
