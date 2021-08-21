d425debfb1db2ae3296b4a96a568b4bce5938b16
Alexandrmrk <alexandrmrk@mail.ru>
2021-08-21 09:07:37 +0300

    feat: обновить дамп, реализовать миграцию по добавлению маркеров

---
+++ server/database/migrations/1629522266634-addMapMarkers.ts
@@ -0,0 +1,52 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+import { jsonToArrayCustom } from '../../utils/helpers';
+import MapCategory from '../entities/MapCategory';
+import MapCategoryMapMarker from '../entities/MapCategoryMapMarker';
+import MapMarker from '../entities/MapMarker';
+
+export class addMapMarkers1629522266634 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const { data } = await jsonToArrayCustom('database/dumps/data.json');
+    const promises = data.map(async (category: any) => {
+      const { district, objects } = category;
+
+      const mapCategory = new MapCategory();
+      mapCategory.title = district;
+      mapCategory.innerName = district;
+
+      const createdMapCategory = await queryRunner.manager.save(mapCategory);
+
+      const markers = objects.map((_: any, i) => ({
+        lat: i,
+        long: i + 1,
+        s2_path: '',
+        shortDescription: _.shortDescription,
+        description: _.description,
+        documents: _.documents,
+        informants: _.informants,
+        name: _.title,
+      }));
+
+      const createdMapMarkers = await queryRunner.manager.save(
+        MapMarker,
+        markers
+      );
+
+      const mapMarkerMapCategoryRelations = createdMapMarkers.map(
+        (createdMapMarker: any) => {
+          const mapCategoryMapMarker = new MapCategoryMapMarker();
+          mapCategoryMapMarker.mapCategory = createdMapCategory;
+          mapCategoryMapMarker.mapMarker = createdMapMarker;
+          return mapCategoryMapMarker;
+        }
+      );
+      await queryRunner.manager.save(mapMarkerMapCategoryRelations);
+    });
+
+    await Promise.all(promises);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+}
