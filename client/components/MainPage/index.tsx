8242410fc36103bf2045e455d0c55236874a89c8
Dmitriy Menliyakhmatov <menliiakhmatovdg@lad24.ru>
2021-08-21 16:21:49 +0300

    feat: удалил текстуру

--- client/components/MainPage/index.tsx
+++ client/components/MainPage/index.tsx
@@ -18,12 +18,12 @@ const MainPage = () => {
       </div>
       <PageHeader />
       <ChooseBlock />
-      <div className="map__container_wrapper">
-        <div className="opacity_container">
-          <div className="container">
-            <MapComponent />
-          </div>
-        </div>
+      {/* <div className="map__container_wrapper"> */}
+      {/*  <div className="opacity_container"> */}
+      <div className="container">
+        <MapComponent />
+        {/*  </div> */}
+        {/* </div> */}
       </div>
       <Footer />
     </>
